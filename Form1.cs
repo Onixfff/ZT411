980a295c73e13bced6979bc9f4613bc8c27c7530
Onixfff <danilnikonov01@mail.ru>
2024-01-29 11:55:52 +0200

    Create Reposit

---
+++ Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Zebra;
+using Zebra.Sdk.Comm;
+using Zebra.Sdk.Comm.Internal;
+
+namespace ZT411
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SendZplOverTcp(string ipAdress)
+        {
+            Connection conn = new TcpConnection(ipAdress, TcpConnection.DEFAULT_ZPL_TCP_PORT);
+
+            try
+            {
+                conn.Open();
+
+                string zplData = "^XA^FO20,20^A0N,25,25^FDЭто тест zpl.^FS^XZ";
+
+                conn.Write(Encoding.UTF8.GetBytes(zplData));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally { conn.Close(); }
+        }
+    }
+}
